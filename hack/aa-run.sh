a23ff744b5ddb18969565e6e3f805ff511d0925a
magicsong <625448253@qq.com>
2018-11-30 18:22:50 +0800

    everyday push

--- hack/aa-run.sh
+++ hack/aa-run.sh
@@ -1,7 +1,7 @@
 #/bin/bash
 
 git add -A -- .
-git commit --quiet --allow-empty-message --file - --all
+git commit --quiet --allow-empty-message --file - --all -m "debug auto commit"
 
 tag=$(git rev-parse --short HEAD)
 image_name=dockerhub.qingcloud.com/magicsong/s2iapiserver:$tag
@@ -9,5 +9,8 @@ image_name=dockerhub.qingcloud.com/magicsong/s2iapiserver:$tag
 docker build -f build/Dockerfile bin/ -t $image_name
 docker push $image_name
 
+rm -f config/apiserver.yaml
 apiserver-boot build config --name s2iapiserver --namespace default --image $image_name --apiserver-args "-v 3 -logtostderr=true" --controller-args "-v 3 -logtostderr=true"
+
+unset https_proxy
 kubectl apply -f config/apiserver.yaml
